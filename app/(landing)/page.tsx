97b38d1be3c3cd01188a550d7a486369cc92d3df
Bagas Saadih <saadihbagas@gmail.com>
2026-01-17 21:47:29 +0700

    Last Update

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,11 @@
 import HeroSection from "./components/home/hero";
 import CategoriesSection from "./components/home/categories";
 import ProductsSection from "./components/home/products";
+import { getAllCategories } from "../services/category.service";
 
-export default function Home() {
+export default async function Home() {
+  await getAllCategories();
+
   return (
     <main>
       <HeroSection />
