f75bb24af54836c8367502718d2f4c21dee1483a
Bagas Saadih <saadihbagas@gmail.com>
2026-01-20 13:37:26 +0700

    lates update

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -2,7 +2,7 @@ export async function fetchAPI<T>(
   endpoint: string,
   options?: RequestInit,
 ): Promise<T> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}%{endpoint}`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${endpoint}`, {
     ...options,
     cache: options?.cache || "no-store", // kita set no-store karena kita ingin mendapat data lebih real time atau lebih updated
   });
@@ -20,4 +20,9 @@ export async function fetchAPI<T>(
   }
 
   return res.json();
+}
+
+export function getImageUrl(path: string) {
+  if (path.startsWith("http")) return path;
+  return `${process.env.NEXT_PUBLIC_API_ROOT}/${path}`;
 }
